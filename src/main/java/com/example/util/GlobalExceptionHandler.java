5ebd7560849ecfd7246e808297eef81b34e0253c
Lupin <syncline139@gmail.com>
2025-03-14 15:55:24 +1000

    Обработка ошибок

---
+++ src/main/java/com/example/util/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.util;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String Exception(Exception exception, Model model) {
+        model.addAttribute("errorPage", exception.getMessage());
+        return "pages/error";
+    }
+}
