6e947ccf7bce81278560e3c7c0bfa07e24b52f3a
dadada <syncline139@gmail.com>
2025-03-29 09:04:03 +1000

    Поиск локации

--- src/main/java/com/example/controllers/WeatherController.java
+++ src/main/java/com/example/controllers/WeatherController.java
@@ -1,16 +1,24 @@
 package com.example.controllers;
 
 import com.example.dao.AuthDao;
+import com.example.dto.response.WeatherResponseDto;
 import com.example.models.Users;
 import com.example.services.AuthServices;
+import com.example.services.WeatherService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.print.DocFlavor;
 
 @Controller
 @RequestMapping("/weather")
@@ -20,6 +28,7 @@ public class WeatherController {
 
     private final AuthDao authDao;
     private final AuthServices authServices;
+    private final WeatherService weatherService;
 
     @GetMapping()
     public String mainScreenPage(HttpServletRequest request, Model model) {
@@ -30,6 +39,32 @@ public class WeatherController {
             model.addAttribute("login", login);
         }
         return "pages/index";
+    }
+
+
+    @PostMapping("/search-results")
+    public String search(@RequestParam("nameCity") String nameCity, Model model, HttpServletRequest request
+            ,RedirectAttributes redirectAttributes) {
+
+        if (nameCity == null || nameCity.trim().isEmpty()) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Город не может быть пустым");
+            return "redirect:/weather";
+        }
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            String login = (String) session.getAttribute("login");
+            model.addAttribute("login", login);
+        }
+
+        WeatherResponseDto search = weatherService.search(nameCity);
+        model.addAttribute("nameCity", search.getName());
+        model.addAttribute("coord", search.getCoord());
+        model.addAttribute("sys", search.getSys());
+
+        return "pages/search-results";
+
+
     }
 
 }
--- src/main/java/com/example/services/WeatherService.java
+++ src/main/java/com/example/services/WeatherService.java
@@ -1,7 +1,49 @@
 package com.example.services;
 
+import com.example.dto.response.WeatherResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
 @Service
+@RequiredArgsConstructor
 public class WeatherService {
+
+
+    private final Environment environment;
+
+    @SneakyThrows
+    public WeatherResponseDto search(String nameCity) {
+        final var API = environment.getProperty("API");
+        if (API == null || API.isBlank()) {
+            throw new IllegalStateException("API ключ не корректный");
+        }
+        String encoderCity = URLEncoder.encode(nameCity, StandardCharsets.UTF_8.toString());
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + encoderCity + "&appid=" + API;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 200) {
+            throw new IOException("Не удалось получить данные о погоде: " + response.statusCode());
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(response.body(), WeatherResponseDto.class);
+    }
 }
