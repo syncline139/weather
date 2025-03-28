2b55cb9eb6ae0543e536914f73c072b9482c01ae
dadada <syncline139@gmail.com>
2025-03-28 19:34:59 +1000

    Криво, но работает поиск локации

--- src/main/java/com/example/controllers/WeatherController.java
+++ src/main/java/com/example/controllers/WeatherController.java
@@ -1,29 +1,30 @@
 package com.example.controllers;
 
 import com.example.dao.AuthDao;
-import com.example.models.Users;
+import com.example.dto.response.WeatherDto;
 import com.example.services.AuthServices;
+import com.example.services.WeatherService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/weather")
 @RequiredArgsConstructor
-
 public class WeatherController {
 
     private final AuthDao authDao;
     private final AuthServices authServices;
+    private final WeatherService weatherService;
 
-    @GetMapping()
+    @GetMapping
     public String mainScreenPage(HttpServletRequest request, Model model) {
-
         HttpSession session = request.getSession(false);
         if (session != null) {
             String login = (String) session.getAttribute("login");
@@ -32,4 +33,10 @@ public class WeatherController {
         return "pages/index";
     }
 
+    @PostMapping
+    public String search(@RequestParam("nameCity") String nameCity, Model model) {
+        WeatherDto weatherDto = weatherService.search(nameCity);
+        model.addAttribute("weatherDto", weatherDto); // Передаем весь объект WeatherDto
+        return "pages/search-results";
+    }
 }
--- src/main/java/com/example/services/WeatherService.java
+++ src/main/java/com/example/services/WeatherService.java
@@ -1,4 +1,50 @@
 package com.example.services;
 
+import com.example.dao.AuthDao;
+import com.example.dto.response.WeatherDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+@RequiredArgsConstructor
 public class WeatherService {
+
+    private final AuthDao authDao;
+    private final Environment environment;
+
+    @SneakyThrows
+    public WeatherDto search(String nameCity) {
+        String apiKey = environment.getProperty("API");
+        if (apiKey == null) {
+            throw new IllegalArgumentException("API ключ не задан");
+        }
+        if (nameCity == null || nameCity.trim().isEmpty()) {
+            throw new IllegalArgumentException("Город не может быть null или пустым");
+        }
+
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + nameCity + "&appid=" + apiKey + "&units=metric";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Ошибка API: " + response.statusCode());
+        }
+
+        String json = response.body();
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, WeatherDto.class);
+    }
 }
